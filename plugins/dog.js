c5fbaa2c0291cfddba878b0253c35743248cb89e
qwer123x <namebetting@gmail.com>
2025-06-23 12:04:06 +0300

    Update dog.js

--- plugins/dog.js
+++ plugins/dog.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.message }, caption: '> *© ᴋᴇᴇɴʟʏ xᴍᴅ ' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.message }, caption: '> *© BEN xᴍᴅ ' }, { quoted: mek });
     } catch (e) {
         console.log(e); // ❯❯ 𝕊𝕚𝕝𝕧𝕒 𝕊𝕡𝕒𝕣𝕜 𝕄𝔻
         reply(`єяяσя ƒєт¢нιηg ∂σg ιмαgє: ${e.message}`);
