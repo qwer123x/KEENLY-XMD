153564b417be5f231bc74f5520b8c033eff6e2ed
qwer123x <namebetting@gmail.com>
2025-06-23 12:36:21 +0300

    Update tools.js

--- plugins/tools.js
+++ plugins/tools.js
@@ -102,7 +102,7 @@ async (conn, mek, m, { from, q, reply }) => {
 
 ${joke.punchline} 😄
 
-> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ SILVA*`;
+> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ben*`;
 
         return reply(jokeMessage);
     } catch (e) {
@@ -167,7 +167,7 @@ cmd({
     let fontResults = data.result.map(font => '*' + font.name + ":*\n" + font.result).join("\n\n");
     
     // Message formatting
-    let message = `*SILVA SPARK FANCY FONTS*:\n\n${fontResults}\n\n> *BY SILVA SPARK*`;
+    let message = `*BEN XMD FANCY FONTS*:\n\n${fontResults}\n\n> *BY SILVA SPARK*`;
 
     // Sending the message with context info
     await conn.sendMessage(
@@ -180,7 +180,7 @@ cmd({
           isForwarded: true,
           forwardedNewsletterMessageInfo: {
             newsletterJid: '120363200367779016@newsletter',
-            newsletterName: 'SILVA SPARK',
+            newsletterName: 'BEN',
             serverMessageId: 143
           }
         }
