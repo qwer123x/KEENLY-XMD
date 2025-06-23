8967e5c85a3b275f44758ba15b7b5a197205469b
qwer123x <namebetting@gmail.com>
2025-06-23 12:33:31 +0300

    Update silva-channelreact.js

--- plugins/silva-channelreact.js
+++ plugins/silva-channelreact.js
@@ -45,12 +45,12 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const channelMeta = await conn.newsletterMetadata("invite", channelId);
         await conn.newsletterReactMessage(channelMeta.id, messageId, emoji);
 
-        return reply(`╭━━━〔 *KEENLY XMD* 〕━━━┈⊷
+        return reply(`╭━━━〔 *BEN XMD* 〕━━━┈⊷
 ┃▸ *Success!* Reaction sent
 ┃▸ *Channel:* ${channelMeta.name}
 ┃▸ *Reaction:* ${emoji}
 ╰────────────────┈⊷
-> *© Pᴏᴡᴇʀᴇᴅ Bʏ KEENLY XMD ♡*`);
+> *© Pᴏᴡᴇʀᴇᴅ Bʏ BEN XMD ♡*`);
     } catch (e) {
         console.error(e);
         reply(`❎ Error: ${e.message || "Failed to send reaction"}`);
