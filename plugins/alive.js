7cd24012d00746ddaad24adaf9f4170af2d3fc43
qwer123x <namebetting@gmail.com>
2025-06-23 11:55:30 +0300

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -13,16 +13,16 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         // Generate system status message
-        const status = `╭━━〔 *✦ 𝙆𝙀𝙀𝙉𝙇𝙔 𝙓𝙈𝘿 ✦* 〕━━┈⊷
+        const status = `╭━━〔 *✦ BEN 𝙓𝙈𝘿 ✦* 〕━━┈⊷
 ┃🦄╭─────────────·๏
 ┃🦄┃• *⏳Uptime*:  ${runtime(process.uptime())} 
 ┃🦄┃• *📟 Ram usage*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}GB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}TB
 ┃🦄┃• *⚙️ HostName*: ${os.hostname()}
-┃🦄┃• *👨‍💻 Owner*: ᴏʙᴇᴅᴛᴇᴄʜ
+┃🦄┃• *👨‍💻 Owner*: BEN
 ┃🦄┃• *🧬 Version*: 6.0.0
-✦ 𝙆𝙀𝙀𝙉𝙇𝙔 𝙓𝙈𝘿 ✦
+✦ BEN 𝙓𝙈𝘿 ✦
 ╰──────────────┈⊷
-> © ✦ 𝙆𝙀𝙀𝙉𝙇𝙔 𝙓𝙈𝘿 ✦`;
+> © ✦ BEN 𝙓𝙈𝘿 ✦`;
 
         // Send the status message with an image
         await conn.sendMessage(from, { 
@@ -34,7 +34,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363416335506023@newsletter',
-                    newsletterName: '𝙆𝙀𝙀𝙉𝙇𝙔 𝙓𝙈𝘿🎄',
+                    newsletterName: 'BEN 𝙓𝙈𝘿🎄',
                     serverMessageId: 143
                 }
             }
