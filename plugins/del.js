9bc355ef0fe540c82d754d1fb63b478bfd48498d
qwer123x <namebetting@gmail.com>
2025-06-23 12:03:07 +0300

    Update del.js

--- plugins/del.js
+++ plugins/del.js
@@ -23,6 +23,6 @@ const key = {
         await conn.sendMessage(m.chat, { delete: key })
 } catch(e) {
 console.log(e);
-reply('💎 ᴋᴇᴇɴʟʏ xᴍᴅ 💎 successful..👨‍💻✅')
+reply('💎 BEN xᴍᴅ 💎 successful..👨‍💻✅')
 } 
 })
