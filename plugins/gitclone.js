3d653a90810df0ea934f2101db6a2dc24777f921
qwer123x <namebetting@gmail.com>
2025-06-23 12:14:32 +0300

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -16,7 +16,7 @@ cmd({
   reply: _0x11eb9d
 }) => {
   if (!_0x216653[0x0]) {
-    return _0x11eb9d("Where is the GitHub link?\n\nExample:\n.gitclone https://github.com/Obedweb/KEENLY-XMD");
+    return _0x11eb9d("Where is the GitHub link?\n\nExample:\n.gitclone https://github.com/qwer123x/KEENLY-XMD");
   }
   if (!/^(https:\/\/)?github\.com\/.+/.test(_0x216653[0x0])) {
     return _0x11eb9d("⚠️ Invalid GitHub link.");
@@ -36,7 +36,7 @@ cmd({
     }
     let _0x49a379 = _0xe6be44.headers.get("content-disposition");
     let _0x481150 = _0x49a379 ? _0x49a379.match(/filename=(.*)/)[0x1] : _0x16a926 + ".zip";
-    _0x11eb9d("*📥𝙺𝙴𝙴𝙽𝙻𝚈 𝚇𝙼𝙳 DOWNLOADING REPOSITORY...*\n\n*REPOSITORY:* " + _0x5194b2 + '/' + _0x16a926 + "\n*FILENAME:* " + _0x481150 + "\n\n> *© 𝙺𝙴𝙴𝙽𝙻𝚈 𝚇𝙼𝙳*");
+    _0x11eb9d("*📥BEN 𝚇𝙼𝙳 DOWNLOADING REPOSITORY...*\n\n*REPOSITORY:* " + _0x5194b2 + '/' + _0x16a926 + "\n*FILENAME:* " + _0x481150 + "\n\n> *© 𝙺𝙴𝙴𝙽𝙻𝚈 𝚇𝙼𝙳*");
     await _0x359a4d.sendMessage(_0x421a7d, {
       'document': {
         'url': _0x936878
@@ -49,7 +49,7 @@ cmd({
         'isForwarded': true,
         'forwardedNewsletterMessageInfo': {
           'newsletterJid': "120363416335506023@newsletter",
-          'newsletterName': "𝐊𝐄𝐄𝐍𝐋𝐘 𝐆𝐈𝐓𝐇𝐔𝐁 𝐂𝐋𝐎𝐍𝐄💀",
+          'newsletterName': "BEN 𝐆𝐈𝐓𝐇𝐔𝐁 𝐂𝐋𝐎𝐍𝐄💀",
           'serverMessageId': 0x8f
         }
       }
